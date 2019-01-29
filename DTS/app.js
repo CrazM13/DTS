bfcbd7db3514887068420a3286930e8f116838ae
CrazM13 <sepulvedajoey13@yahoo.com>
2019-01-28 21:29:06 -0500

    Worked on log in

--- DTS/app.js
+++ DTS/app.js
@@ -1,4 +1,4 @@
-const http = require('http');
+const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const mustache = require('mustache');
@@ -9,27 +9,39 @@ const router = express.Router();
 
 const port = 5000;
 
-app.set("view engine", "http");
+app.engine('mustache', mustache_express());
+app.set('view engine', 'mustache');
 
-app.use(express.static(path.join(__dirname, "/views")));
+app.set('views', path.join(__dirname, "/views"));
 app.use(express.static(path.join(__dirname, "/public")));
 
 
 
 router.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, '/index.html'));
+	//res.sendFile(path.join(__dirname, '/index.html'));
+	res.render('index', {PAGE:"<h3>MUSTACHE</h3>"});
 });
 
 router.get('/search', (req, res) => {
-	
+	res.render('index', { PAGE: "<h3>MUSTACHE</h3>" });
 });
 
 router.get('/input', (req, res) => {
-	console.log("");
+	res.render('index', { PAGE: "<h3>MUSTACHE</h3>" });
 });
 
 router.get('/login', (req, res) => {
-	
+	var email = req.query.email;
+	var password = req.query.password;
+	var user = req.query.user;
+	if (email != undefined && password != undefined) {
+		console.log(encrypt(password, email));
+		res.redirect('/login?user=' + email);
+	} else if (user != undefined) {
+		res.render('index', { PAGE: "<h3>MUSTACHE</h3>" });
+	} else {
+		res.render('login', { PAGE: "<h3>MUSTACHE</h3>" });
+	}
 });
 
 router.get('/signup', (req, res) => {
@@ -40,3 +52,14 @@ app.use('/', router);
 app.listen(port, () => {
 	console.log("Server is running on port " + port.toString());
 });
+
+// Helper Functions
+
+function encrypt(rawPassword, key) {
+	var newPassword;
+	var length = Math.max(rawPassword.length, key.length)
+	for (i = 0; i < length; i++) {
+		newPassword += String.fromCharCode(rawPassword.charCodeAt(i % rawPassword.length) + key.charCodeAt(i % key.length));
+	}
+	return newPassword;
+}
